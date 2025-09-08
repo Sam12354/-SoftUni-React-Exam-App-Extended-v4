fd9f39297eb7ff1253ed6d80fae26ab44adade98
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-09-08 21:28:55 +0300

    config json to fix .js problem and use custom Auth Request to access req.user

---
+++ server/src/controllers/shoppingCartController.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response } from "express";
+import { getErrorMassage } from '../utils/errorUtils.js';
+import { shoppingCartService } from "../services/ShoppingCartService.ts";
+import { Types } from 'mongoose'
+// trqbva da import Request, Response za6toto ts ne znae req, res kakuv type sa
+
+
+const shoppingCartController = Router()
+
+interface AuthRequest extends Request {
+    user?: {
+      _id: Types.ObjectId;
+    };
+}
+// you use this custom AuthRequest type whenever you need to access req.user
+
+shoppingCartController.get('/', async (req: AuthRequest, res: Response) => {
+
+    const userId = req.user?._id
+
+    if(!userId){
+        return res.status(401).json({ error: 'User not authenticated' });
+    }
+
+    try {
+        const items = await shoppingCartService(userId)
+        res.json(items)
+    } catch (error) {
+        res.status(400).json({ error: getErrorMassage(error) })
+    }
+
+})
+
+shoppingCartController.post('/', async (req: Request, res: Response) => {
+
+})
