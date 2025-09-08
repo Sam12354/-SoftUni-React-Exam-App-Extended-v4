ca0a9b97535096727958cb36eeecea26e482c9ed
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-09-08 21:47:43 +0300

    convert route params and userId to ObjectId for shopping cart POST in TS

--- server/src/controllers/shoppingCartController.ts
+++ server/src/controllers/shoppingCartController.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { getErrorMassage } from '../utils/errorUtils.js';
-import { shoppingCartServiceGet } from "../services/shoppingCartService.js";
+import { shoppingCartService, shoppingCartServiceGet } from "../services/shoppingCartService.js";
 import { Types } from 'mongoose'
 // trqbva da import Request, Response za6toto ts ne znae req, res kakuv type sa
 
@@ -31,6 +31,19 @@ shoppingCartController.get('/', async (req: AuthRequest, res: Response) => {
 
 })
 
-shoppingCartController.post('/', async (req: Request, res: Response) => {
+shoppingCartController.post('/', async (req: AuthRequest, res: Response) => {
+    const userId = req.user?._id
+    const itemId = new Types.ObjectId(req.params.itemId)
+    // string but TS expects an objectId
 
+    if(!userId){
+        return res.status(401).json({ error: 'User not authenticated' });
+    }
+
+    try {
+        const items = await shoppingCartService(userId, itemId)
+        res.json(items)
+    } catch (error) {
+        res.status(400).json({ error: getErrorMassage(error) })
+    }
 })
